0d6dd7e8e2c2ef6724bd21301209a17dbb576c01
Adelina Protopopova <protdelya@icloud.com>
2025-07-03 23:10:02 +1000

    Добавлен маршрут /static и /dynamic

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const express = require("express");
  
 const app = express();
+
 app.get("/", function(request, response){
      
     response.send("<h1>Привет Октагон!</h1>");
+});
+
+app.get("/static", function(request, response) {
+    response.json({
+        header: "Hello",
+        body: "Octagon NodeJS Test"
+    });
+});
+
+app.get("/dynamic", function(request, response) {
+    const a = request.query.a;
+    const b = request.query.b;
+    const c = request.query.c;
+
+    const numA = Number(a);
+    const numB = Number(b);
+    const numC = Number(c);
+
+    if (isNaN(numA) || isNaN(numB) || isNaN(numC)) {
+        response.json({ header: "Error" });
+    } else {
+        const result = (numA * numB * numC) / 3;
+        response.json({
+            header: "Calculated",
+            body: result
+        });
+    }
 });
 app.listen(3000);
