816d14db1f74553cb4f0207a1b5e2741e96ab32b
delya-jpg <protdelya@icloud.com>
2025-07-06 22:19:22 +1000

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
- 
-const app = express();
-app.get("/", function(request, response){
-     
-    response.send("<h1>Привет Октагон!</h1>");
-});
-app.listen(3000);
