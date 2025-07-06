36de79dd0e9174842f3061f2769851528d841c9e
Adelina Protopopova <protdelya@icloud.com>
2025-07-06 23:26:55 +1000

    Обновление бота с новыми командами

--- bot.js
+++ bot.js
@@ -3,7 +3,24 @@ const token = '8113565640:AAFuUDu8lY3Cwjjp_nzLSnndZtYASAU9pCc';
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Привет, октагон!');
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Привет, октагон!');
 });
+
+bot.onText(/\/help/, (msg) => {
+  const helpText = `
+Доступные команды:
+/site - ссылка на сайт октагона
+/creator - ФИО разработчика
+  `;
+  bot.sendMessage(msg.chat.id, helpText);
+});
+
+bot.onText(/\/site/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'https://octagon.ru');
+});
+
+bot.onText(/\/creator/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Протопопова Аделина Александровна');
+});
+
